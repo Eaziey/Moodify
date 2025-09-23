50af352bcb956e5f8719576fdd4c27675e435d58
Letlhogonolo Rakgantsho <107153213+Eaziey@users.noreply.github.com>
2025-09-23 14:39:19 +0000

    Added Register page starting template

---
+++ frontend/Moodify_UI/src/components/signInForm.vue
@@ -0,0 +1,55 @@
+
+<template>
+
+<div class="min-vh-100 w-100 border border-danger d-flex justify-content-center align-items-center">
+    <form class="border d-flex flex-column p-5 h-75 w-75">
+        <h1 class="text-primary align-self-center mb-5">Register</h1>
+
+        <div class="input-group align-self-center my-3 input-w">
+            <span class="input-group-text p-2">@</span>
+            <input type="text" class="form-control p-2" placeholder="Username"/>
+        </div>
+        <div class="input-group align-self-center my-3 input-w">
+            <span class="input-group-text p-2">@</span>
+            <input type="email" class="form-control p-2" placeholder="Email"/>
+        </div>
+        <div class="input-group align-self-center my-3 input-w">
+            <span class="input-group-text p-2">@</span>
+            <input type="password" class="form-control border-end-0 p-2" placeholder="Password"/>
+            <span class="input-group-text border-start-0 bg-transparent hover-pointer p-2">#</span>
+        </div>
+        <div class="input-group align-self-center my-3 input-w">
+            <span class="input-group-text p-2">@</span>
+            <input type="password" class=" form-control border-end-0 p-2" placeholder="Confirm Password"/>
+            <span class="input-group-text border-start-0 bg-transparent hover-pointer p-2">#</span>
+        </div>
+
+        <button type="submit" class="btn btn-outline-success rounded-2 w-50 align-self-center mt-4">Sign In</button>
+        
+        <div class="d-flex flex-row justify-content-center mt-5">
+            <p class="align-self-center">Or</p>
+        </div>
+
+        <div class="d-flex flex-row"> 
+            <button class="btn btn-outline-success mt-3 me-3 rounded-2 w-50 align-self-center">Sportify</button>
+            <button class="btn btn-outline-dark mt-3 me-3 rounded-2 w-50 align-self-center">Google</button>
+            <button class="btn btn-outline-primary mt-3 me-3 rounded-2 w-50 align-self-center">Facebook</button>
+        </div>
+    </form>
+</div>
+
+</template>
+
+<script>
+export default{
+    name: 'SignInForm'
+}
+
+</script>
+
+<style scoped>
+.input-w{
+    width : 85%;
+}
+
+</style>
---
+++ frontend/Moodify_UI/src/pages/loginRegister.vue
@@ -0,0 +1,29 @@
+<template>
+    
+    <div class="w-100 h-100 border border-1 border-dark d-flex flex-row justify-content-center">
+        <div class="container w-100 border border-2 border-danger text-center p-2">
+            <h1 class="text-primary">Moodify</h1>
+        </div>
+        <div class="container">
+            <SignInForm/>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+import SignInForm from '../components/signInForm.vue';
+
+export default {
+    name : "LoginRegister",
+    components: {
+        SignInForm
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
